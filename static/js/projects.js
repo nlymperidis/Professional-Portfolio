f70f10f90837af9adc8a1a9dc9cd1d3d1849b9f8
nikos <nikos.lyberidis@gmail.com>
2024-06-02 20:46:13 +0300

    Updated projects page.Updated project styles

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,17 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const projectsList = document.getElementById('projects-list');
 
+    // Define an array of images
+    const projectImages = [
+        "static/assets/img/projects/project1.jpg",
+        "static/assets/img/projects/project2.jpg",
+        "static/assets/img/projects/project3.jpg",
+        // Add more images here as needed
+    ];
+
     // Fetch GitHub repositories
     fetch('https://api.github.com/users/nlymperidis/repos')
         .then(response => response.json())
         .then(repos => {
-            repos.forEach(repo => {
+            repos.forEach((repo, index) => {
                 const projectElement = document.createElement('div');
                 projectElement.classList.add('project-wrapper');
 
+                const imageUrl = projectImages[index % projectImages.length]; // Cycle through images
+
                 projectElement.innerHTML = `
                     <figure>
-                        <img src="static/assets/img/programming.jpg" alt="Programming" />
+                        <img src="${imageUrl}" alt="Project Image" />
                     </figure>
                     <div class="project-body">
                         <h2>${repo.name}</h2>
